55d9707266646503f40d4b2b47fe34248f74d10b
rezomusic <matthewrector1717@gmail.com>
2022-12-19 18:12:32 -0500

    Added titanium tools

--- src/main/java/com/rezomediaproductions/metallurgicdiscovery/items/ItemsMain.java
+++ src/main/java/com/rezomediaproductions/metallurgicdiscovery/items/ItemsMain.java
@@ -2,13 +2,16 @@ package com.rezomediaproductions.metallurgicdiscovery.items;
 
 import com.rezomediaproductions.metallurgicdiscovery.util.CreativeModeTabs;
 import com.rezomediaproductions.metallurgicdiscovery.MetallurgicDiscovery;
-import net.minecraft.world.item.Item;
+import com.rezomediaproductions.metallurgicdiscovery.util.MDToolTiers;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemsMain {
+
+    // TITANIUM ORE ITEMS
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MetallurgicDiscovery.MOD_ID);
 
@@ -18,6 +21,18 @@ public class ItemsMain {
 
     public static final RegistryObject<Item> TITANIUM_INGOT =
             ITEMS.register("titanium_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTabs.CREATIVE_MODE_TAB)));
+
+    // TITANIUM TOOLS
+    public static final RegistryObject<Item> TITANIUM_SWORD =
+            ITEMS.register("titanium_sword", () -> new SwordItem(MDToolTiers.TITANIUM_TIER, 3, 1f, new Item.Properties().tab(CreativeModeTabs.CREATIVE_MODE_TAB)));
+    public static final RegistryObject<Item> TITANIUM_PICKAXE =
+            ITEMS.register("titanium_pickaxe", () -> new PickaxeItem(MDToolTiers.TITANIUM_TIER, 2, 1f, new Item.Properties().tab(CreativeModeTabs.CREATIVE_MODE_TAB)));
+    public static final RegistryObject<Item> TITANIUM_AXE =
+            ITEMS.register("titanium_axe", () -> new AxeItem(MDToolTiers.TITANIUM_TIER, 3, 1f, new Item.Properties().tab(CreativeModeTabs.CREATIVE_MODE_TAB)));
+    public static final RegistryObject<Item> TITANIUM_SHOVEL =
+            ITEMS.register("titanium_shovel", () -> new ShovelItem(MDToolTiers.TITANIUM_TIER, 1, 1f, new Item.Properties().tab(CreativeModeTabs.CREATIVE_MODE_TAB)));
+    public static final RegistryObject<Item> TITANIUM_HOE =
+            ITEMS.register("titanium_hoe", () -> new HoeItem(MDToolTiers.TITANIUM_TIER, 1, 1f, new Item.Properties().tab(CreativeModeTabs.CREATIVE_MODE_TAB)));
 
     public static void register(IEventBus eventBus)
     {
