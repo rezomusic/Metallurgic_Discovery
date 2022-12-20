27ff81eaae1c9e599341563dfa3d8dda8051702e
rezomusic <matthewrector1717@gmail.com>
2022-12-20 01:29:13 -0500

    Added overworld alloy blocks and items

--- src/main/java/com/rezomediaproductions/metallurgicdiscovery/blocks/BlocksMain.java
+++ src/main/java/com/rezomediaproductions/metallurgicdiscovery/blocks/BlocksMain.java
@@ -22,6 +22,9 @@ public class BlocksMain {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, MetallurgicDiscovery.MOD_ID);
 
+    // -----OVERWORLD ORES----- //
+
+    // Titanium Blocks
     public static final RegistryObject<Block> TITANIUM_ORE =
             registerBlock("titanium_ore",
                         () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
@@ -29,13 +32,59 @@ public class BlocksMain {
 
     public static final RegistryObject<Block> DEEPSLATE_TITANIUM_ORE =
             registerBlock("deepslate_titanium_ore",
-                        () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                        () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
                         CreativeModeTabs.CREATIVE_MODE_TAB);
 
     public static final RegistryObject<Block> TITANIUM_BLOCK =
             registerBlock("titanium_block",
                         () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(5f).requiresCorrectToolForDrops()),
                         CreativeModeTabs.CREATIVE_MODE_TAB);
+
+    // Mythril Ore Blocks
+    public static final RegistryObject<Block> MYTHRIL_ORE =
+            registerBlock("mythril_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(4f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                    CreativeModeTabs.CREATIVE_MODE_TAB);
+
+    public static final RegistryObject<Block> DEEPSLATE_MYTHRIL_ORE =
+            registerBlock("deepslate_mythril_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                    CreativeModeTabs.CREATIVE_MODE_TAB);
+
+    // Chromium Ore Blocks
+    public static final RegistryObject<Block> CHROMIUM_ORE =
+            registerBlock("chromium_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(4f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                    CreativeModeTabs.CREATIVE_MODE_TAB);
+
+    public static final RegistryObject<Block> DEEPSLATE_CHROMIUM_ORE =
+            registerBlock("deepslate_chromium_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                    CreativeModeTabs.CREATIVE_MODE_TAB);
+
+    // Vanadium Ore Blocks
+    public static final RegistryObject<Block> VANADIUM_ORE =
+            registerBlock("vanadium_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(4f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                    CreativeModeTabs.CREATIVE_MODE_TAB);
+
+    public static final RegistryObject<Block> DEEPSLATE_VANADIUM_ORE =
+            registerBlock("deepslate_vanadium_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                    CreativeModeTabs.CREATIVE_MODE_TAB);
+
+    // Celestite Ore Blocks
+    public static final RegistryObject<Block> CELESTITE_ORE =
+            registerBlock("celestite_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(4f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                    CreativeModeTabs.CREATIVE_MODE_TAB);
+
+    public static final RegistryObject<Block> DEEPSLATE_CELESTITE_ORE =
+            registerBlock("deepslate_celestite_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3,7)),
+                    CreativeModeTabs.CREATIVE_MODE_TAB);
+
+
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab)
     {
